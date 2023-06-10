ebec691f864ce44ba072e366eadc2615fe9837ab
Gibsol <gibsol75@gmail.com>
2023-06-10 11:43:40 +0300

    Update the server_logic module
    - Refactor the http_request and create a separate function for it.
    - Implement sending an HTML file from the server.

--- src/server_logic.rs
+++ src/server_logic.rs
@@ -1,10 +1,24 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::TcpStream,
 };
 
 pub fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
+    let http_request = get_http_request(buf_reader);
+
+    println!("Request: {:#?}", http_request);
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("index.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn get_http_request(buf_reader: BufReader<&mut TcpStream>) -> Vec<String> {
     let http_request: Vec<_> = buf_reader
         .lines()
         // todo: error handling
@@ -12,5 +26,5 @@ pub fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", http_request);
+    http_request
 }
