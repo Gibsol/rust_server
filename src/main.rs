6921d6147c0f53745733933fbac2e78c34a45b23
Gibsol <gibsol75@gmail.com>
2023-06-10 13:02:40 +0300

    Implement the threading mod in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 mod server_logic;
+mod threading;
 
 use server_logic::handle_connection;
 use std::net::TcpListener;
+use threading::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
