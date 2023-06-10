0741f18729c539428fe377c0f0fcaef6f093e9a7
Gibsol <gibsol75@gmail.com>
2023-06-10 11:04:35 +0300

    Handle_connection function in the server_logic mod
    At the moment, this function simply prints out the body of a HTTP
    request.

---
+++ src/server_logic.rs
@@ -0,0 +1,16 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::TcpStream,
+};
+
+pub fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        // todo: error handling
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+}
