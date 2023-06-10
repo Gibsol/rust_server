2a339e0e7b556c89c4c21e3d1cbd8b01fe4cff64
Gibsol <gibsol75@gmail.com>
2023-06-10 12:12:39 +0300

    Implement sending a 404.html file if NOT FOUND

--- src/server_logic.rs
+++ src/server_logic.rs
@@ -6,22 +6,24 @@ use std::{
 
 pub fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request = get_http_request(buf_reader);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {:#?}", http_request);
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
 
-fn get_http_request(buf_reader: BufReader<&mut TcpStream>) -> Vec<String> {
+fn _get_http_request(buf_reader: BufReader<&mut TcpStream>) -> Vec<String> {
     let http_request: Vec<_> = buf_reader
         .lines()
-        // todo: error handling
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
